6e9b78e5fe209894fb432e6ccfdd70da66d08f81
Mazed1413 <mamazed1314@gmail.com>
2024-03-16 22:47:37 +0600

    update design

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -14,9 +14,9 @@ const Hero = () => {
             Discover an exceptional cooking class tailored for you!
           </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            In the bustling symphony of the kitchen, where pots simmer, knives
+            chop, and aromas dance in the air, there lies an artistry waiting to
+            be explored, a canvas of flavors waiting to be painted.
           </p>
           <div className="flex gap-10 justify-center">
             <button className="btn bg-green-400 border-0 rounded-3xl font-semibold">
