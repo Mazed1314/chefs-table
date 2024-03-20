a7da573688e8fc3c018f5340c91d6bca5937ce5b
Mazed1413 <mamazed1314@gmail.com>
2024-03-20 19:28:12 +0600

    add a component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import NavBar from "./component/navBar/NavBar";
 import Hero from "./component/hero/Hero";
-import Card from "./component/card/Card";
-// import CookingCard from "./component/cooking-card/CookingCard";
+import CookingCard from "./component/cooking-card/CookingCard";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -63,114 +62,15 @@ function App() {
       <div className="m-4 md:mx-20 my-4">
         <NavBar></NavBar>
         <Hero></Hero>
-        {/* --------------------------Our Recipes--------------------- */}
-        <div>
-          <div className="text-center p-4 md:px-40">
-            <h1 className="text-3xl md:text-4xl py-2 font-semibold">
-              Our Recipes
-            </h1>
-            <p className="text-gray-500">
-              Our Recipes is more than just a collection of instructions; it's a
-              testament to our culinary journey, a canvas where flavors,
-              traditions, and creativity converge to create memorable
-              experiences. Each recipe encapsulates not just ingredients and
-              methods, but stories of heritage, innovation, and shared moments
-              around the table.
-            </p>
-          </div>
-          <div className="md:p-2 md:flex">
-            <div className="grid grids-col-1 md:grid-cols-2 gap-5 md:w-7/12 my-2">
-              {cards.map((card) => {
-                return (
-                  <Card
-                    key={card.recipe_id}
-                    card={card}
-                    handleCook={handleCook}
-                  ></Card>
-                );
-              })}
-            </div>
-            <div className="want-to-cook-container md:w-5/12">
-              <div className="border rounded-2xl md:mx-4 p-4 my-2">
-                <h1 className="text-2xl font-bold text-center my-3">
-                  Want to cook: {cook.length}
-                  <div className="text-center w-full flex justify-center py-4">
-                    <hr className="h-1 w-2/3" />
-                  </div>
-                </h1>
-                <table className="bg-gray-100 w-full">
-                  <thead className="py-2 bg-stone-300">
-                    <tr>
-                      <th>Sl</th>
-                      <th>Name</th>
-                      <th>Time</th>
-                      <th>Calories</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {cook.map((item, index) => (
-                      <>
-                        <tr className="text-gray-500">
-                          <td className="px-4">{index + 1}</td>
-                          <td>{item.recipe_name}</td>
-                          <td>{item.preparing_time} min</td>
-                          <td>{item.calories} calories</td>
-                          <td>
-                            <a
-                              onClick={() =>
-                                handlePreparing(item, item.recipe_id)
-                              }
-                              className="btn btn-sm bg-green-400 rounded-2xl py-2 px-4 text-white font-semibold"
-                            >
-                              Preparing
-                            </a>
-                          </td>
-                        </tr>
-                      </>
-                    ))}
-                  </tbody>
-                </table>
-                <h1 className="text-2xl font-bold text-center my-3">
-                  Currently cooking: {currentCook.length}
-                  <div className="text-center w-full flex justify-center py-4">
-                    <hr className="h-1 w-2/3" />
-                  </div>
-                </h1>
-                <table className="w-full">
-                  <thead className="py-2 bg-stone-300">
-                    <tr>
-                      <th>Sl </th>
-                      <th>Name</th>
-                      <th>Time</th>
-                      <th>Calories</th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-gray-100 ">
-                    {currentCook.map((item, index) => (
-                      <>
-                        <tr className="text-gray-500">
-                          <td className="p-4">{index + 1}</td>
-                          <td>{item.recipe_name}</td>
-                          <td>{item.preparing_time}min</td>
-                          <td>{item.calories}calories</td>
-                        </tr>
-                      </>
-                    ))}
-                  </tbody>
-                  <tfoot>
-                    <tr className="text-gray-600 font-semibold">
-                      <td></td>
-                      <td></td>
-                      <td>Total Time : {timeCount}min</td>
-                      <td>Total Calories : {caloriesCount}calories</td>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CookingCard
+          cook={cook}
+          handlePreparing={handlePreparing}
+          cards={cards}
+          handleCook={handleCook}
+          currentCook={currentCook}
+          timeCount={timeCount}
+          caloriesCount={caloriesCount}
+        ></CookingCard>
       </div>
     </>
   );
--- src/component/cooking-card/CookingCard.jsx
+++ src/component/cooking-card/CookingCard.jsx
@@ -1,59 +1,116 @@
-const CookingCard = ({ cook }) => {
+import Card from "../card/Card";
+const CookingCard = ({
+  cook,
+  cards,
+  handlePreparing,
+  handleCook,
+  currentCook,
+  timeCount,
+  caloriesCount,
+}) => {
   return (
-    // <div className="border rounded-2xl md:mx-4 p-4 my-2">
-    //   <h1 className="text-2xl font-bold text-center my-3">
-    //     Want to cook: 01
-    //     <div className="text-center w-full flex justify-center py-4">
-    //       <hr className="h-1 w-2/3" />
-    //     </div>
-    //   </h1>
-    //   <table className="w-full">
-    //     <thead className="py-2">
-    //       <tr>
-    //         <th colSpan={2}>Name</th>
-    //         <th>Time</th>
-    //         <th>Calories</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       <tr>
-    //         <td>01</td>
-    //         <td>Banana Smoothie</td>
-    //         <td>15 min</td>
-    //         <td>200 Calories</td>
-    //         <td>
-    //           <a
-    //             href=""
-    //             className="bg-green-400 rounded-full py-2 px-4 text-white font-semibold"
-    //           >
-    //             <i className="fa-solid fa-utensils"></i>
-    //           </a>
-    //         </td>
-    //       </tr>
-    //       ;
-    //     </tbody>
-    //   </table>
-    //   <h1 className="text-2xl font-bold text-center my-3">
-    //     Currently cooking: 02
-    //     <div className="text-center w-full flex justify-center py-4">
-    //       <hr className="h-1 w-2/3" />
-    //     </div>
-    //   </h1>
-    //   <table className="w-full">
-    //     <tr>
-    //       <th colSpan={2}>Name</th>
-    //       <th>Time</th>
-    //       <th>Calories</th>
-    //     </tr>
-    //     <tr>
-    //       <td>01</td>
-    //       <td>Banana Smoothie</td>
-    //       <td>15 min</td>
-    //       <td>200 Calories</td>
-    //     </tr>
-    //   </table>
-    // </div>
-    
+    <div>
+      <div className="text-center p-4 md:px-40">
+        <h1 className="text-3xl md:text-4xl py-2 font-semibold">Our Recipes</h1>
+        <p className="text-gray-500">
+          Our Recipes is more than just a collection of instructions; it is a
+          testament to our culinary journey, a canvas where flavors, traditions,
+          and creativity converge to create memorable experiences. Each recipe
+          encapsulates not just ingredients and methods, but stories of
+          heritage, innovation, and shared moments around the table.
+        </p>
+      </div>
+      <div className="md:p-2 md:flex">
+        <div className="grid grids-col-1 md:grid-cols-2 gap-5 md:w-7/12 my-2">
+          {cards.map((card) => {
+            return (
+              <Card
+                key={card.recipe_id}
+                card={card}
+                handleCook={handleCook}
+              ></Card>
+            );
+          })}
+        </div>
+        <div className="want-to-cook-container md:w-5/12">
+          <div className="border rounded-2xl md:mx-4 p-4 my-2">
+            <h1 className="text-2xl font-bold text-center my-3">
+              Want to cook: {cook.length}
+              <div className="text-center w-full flex justify-center py-4">
+                <hr className="h-1 w-2/3" />
+              </div>
+            </h1>
+            <table className="bg-gray-100 w-full">
+              <thead className="py-2 bg-stone-300">
+                <tr>
+                  <th>Sl</th>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Calories</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cook.map((item, index) => (
+                  <>
+                    <tr className="text-gray-500">
+                      <td className="px-4">{index + 1}</td>
+                      <td>{item.recipe_name}</td>
+                      <td>{item.preparing_time} min</td>
+                      <td>{item.calories} calories</td>
+                      <td>
+                        <a
+                          onClick={() => handlePreparing(item, item.recipe_id)}
+                          className="btn btn-sm bg-green-400 rounded-2xl py-2 px-4 text-white font-semibold"
+                        >
+                          Preparing
+                        </a>
+                      </td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
+            <h1 className="text-2xl font-bold text-center my-3">
+              Currently cooking: {currentCook.length}
+              <div className="text-center w-full flex justify-center py-4">
+                <hr className="h-1 w-2/3" />
+              </div>
+            </h1>
+            <table className="w-full">
+              <thead className="py-2 bg-stone-300">
+                <tr>
+                  <th>Sl </th>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Calories</th>
+                </tr>
+              </thead>
+              <tbody className="bg-gray-100 ">
+                {currentCook.map((item, index) => (
+                  <>
+                    <tr className="text-gray-500">
+                      <td className="p-4">{index + 1}</td>
+                      <td>{item.recipe_name}</td>
+                      <td>{item.preparing_time}min</td>
+                      <td>{item.calories}calories</td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr className="text-gray-600 font-semibold">
+                  <td></td>
+                  <td></td>
+                  <td>Total Time : {timeCount}min</td>
+                  <td>Total Calories : {caloriesCount}calories</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
